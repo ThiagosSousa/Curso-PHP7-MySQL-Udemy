16ea1da0916e9d069d5660150959923d8cc7fecb
ThiagosSousa <thiagodesiqueirasousa@gmail.com>
2022-05-05 23:13:07 -0300

    Projeto do módulo de orientação a objetos em PHP.

---
+++ ProjetoAppSendMail/processa_envio.php
@@ -0,0 +1,10 @@
+<?php
+    
+    require "../../../../app_send_mail/bibliotecas/PHPMAiler/Exception.php";
+    require "../../../../app_send_mail/bibliotecas/PHPMAiler/OAuth.php";
+    require "../../../../app_send_mail/bibliotecas/PHPMAiler/PHPMAiler.php";
+    require "../../../../app_send_mail/bibliotecas/PHPMAiler/POP3.php";
+    require "../../../../app_send_mail/bibliotecas/PHPMAiler/SMTP.php";
+
+    require "../../../../app_send_mail/processa_envio.php"
+?>
